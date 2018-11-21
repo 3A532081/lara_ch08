eb64f57f275bb08f09bc12c8eb9499100182b4ec
3A532081 <tanshui601@gmail.com>
2018-11-21 20:29:26 +0800

    修改 BoardController 之 getIndex()方法

--- app/Http/Controllers/BoardController.php
+++ app/Http/Controllers/BoardController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Score;
 class BoardController extends Controller
 {
     public function getIndex()
     {
-        return view('board');
+        $scores=Score::orderByTotal()
+            ->orderBySubject()->get();
+        $data=['scores'=>$scores];
+        return view('board',$data);
     }
 }
